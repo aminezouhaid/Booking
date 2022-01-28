f9c7197fb7b24095824ccf1c55f72e219d2d2364
zakaria444 <elismailizakariae@gmail.com>
2022-01-28 09:20:43 +0100

    nettoyage file

--- routes/hotels.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const {
-    addhotel
-  } = require("../controllers/Chotel");
-router.post('/add', async(req,res)=>{
-    await addhotel(req.body,res);
-});
-module.exports = router;
