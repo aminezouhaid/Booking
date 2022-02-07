fe166f2f6919f862529657ba60d786c2f4c9b17c
zakaria444 <elismailizakariae@gmail.com>
2022-02-07 16:27:36 +0100

    add router etoile

--- routes/hotels.js
+++ routes/hotels.js
@@ -16,6 +16,7 @@ const {
 const {
   creatHotelImages,
   getImagesByHotel,
+  getHoteletoiles,
   getImage
 } = require("../controllers/HotelsImagesController");
 
@@ -26,6 +27,10 @@ router.post("/upload", upload.single('image'), userAuth, checkRole(['admin']), c
 router.get("/imageByHotel/:HotelId", userAuth, checkRole(['admin']), getImagesByHotel);
 
 router.get("/", userAuth, checkRole(['admin']), getHotels);
+
+router.get('/etoile/:hoteletoile', async(req,res)=>{
+  await getHoteletoiles(req,res);
+});
 router.get("/:hotelId", userAuth, checkRole(['admin']), getHotel);
 router.patch("/:hotelId", userAuth, checkRole(['admin']), updateHotel);
 router.delete("/:hotelId", userAuth, checkRole(['admin']), deletHotel);
