01dfff87644fd060d6c04acfc3177b456206ae43
zakaria444 <elismailizakariae@gmail.com>
2022-02-10 10:00:51 +0100

    v1 function get date

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -47,13 +47,24 @@ const updatebooking = async (req,res)=> {
 };
 
 
+/*                   get bookinge par date                                        */
 
+const getdate = async (req, res) => {
+      
+  try {
+    const bookings = await booking.find().populate('user_id');
+    res.status(200).json({success: true , data: bookings})
+  }catch(error){
+    res.status(404).json({success: false , data: [], error: error})
+  }
+}
 
 
 
 module.exports = {
     bookingproprietair,
     updatebooking,
-    getbooking
+    getbooking,
+    getdate
     
     };
